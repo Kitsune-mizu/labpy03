1aa8489192a32f9b4f04fe4a32b4b8fd22682e3f
kitsune-mizu <kitsune-mizu>
2024-11-04 12:08:09 +0700

    latihanlatihan2.py2.py

--- latihan2.py
+++ latihan2.py
@@ -5,7 +5,6 @@ total_laba = 0
 # Bulan 1 & 2: Belum ada laba
 for bulan in range(1, 3):
     print(f"laba bulan ke- {bulan} sebesar: {laba}")
-
 # Bulan 3: Laba 1%
 laba = 0.1 * modal_awal
 print(f"laba bulan ke- 3 sebesar: {laba}")
@@ -20,7 +19,6 @@ laba = 0.5 * modal_awal
 for bulan in range(5, 8):
     print(f"laba bulan ke- {bulan} sebesar: {laba}")
     total_laba += laba
-
 # Bulan 8: Laba 3%
 laba = 0.3 * modal_awal
 print(f"laba bulan ke- 8 sebesar: {laba}")
