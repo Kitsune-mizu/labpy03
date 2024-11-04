fb986cb4f1a355a99f48d233f79734a4c1e1f292
kitsune-mizu <kitsune-mizu>
2024-11-04 11:42:11 +0700

    latihan2.py

--- latihan1.py
+++ latihan1.py
@@ -1,18 +1,11 @@
 import random
 
-def random_num(n):
-  numbers = []
-  for i in range(n):
-    number = random.random()  # Menghasilkan bilangan acak antara 0 dan 1
-    while number >= 0.5:  # Ulangi hingga mendapatkan bilangan yang lebih kecil dari 0.5
-      number = random.random()
-    numbers.append(number)
-  return numbers
-# Meminta input dari pengguna untuk jumlah bilangan acak
-n = int(input("Masukkan nilai N: "))
-# Menghasilkan bilangan acak
-random_numbers = random_num(n)
-# Menampilkan bilangan acak
-for i, number in enumerate(random_numbers):
-  print(f"data ke: {i+1} => {number}")
-print("Selesai") 
+N = int(input("Masukkan nilai N: "))
+
+for i in range(1, N + 1):
+  angka_acak = random.random()  # Menghasilkan angka acak antara 0 dan 1
+  while angka_acak >= 0.5:  # Ulangi hingga angka_acak < 0.5
+    angka_acak = random.random()
+  print(f"data ke: {i} => {angka_acak}")
+
+print("Selesai")
---
+++ latihan2.py
@@ -0,0 +1,29 @@
+modal_awal = 100000000
+laba = 0
+total_laba = 0
+
+# Bulan 1 & 2: Belum ada laba
+for bulan in range(1, 3):
+    print(f"laba bulan ke- {bulan} sebesar: {laba}")
+
+# Bulan 3: Laba 1%
+laba = 0.1 * modal_awal
+print(f"laba bulan ke- 3 sebesar: {laba}")
+total_laba += laba
+
+# Bulan 4: Laba 1%
+print(f"laba bulan ke- 4 sebesar: {laba}")
+total_laba += laba
+
+# Bulan 5 - 7: Laba 5%
+laba = 0.5 * modal_awal
+for bulan in range(5, 8):
+    print(f"laba bulan ke- {bulan} sebesar: {laba}")
+    total_laba += laba
+
+# Bulan 8: Laba 3%
+laba = 0.3 * modal_awal
+print(f"laba bulan ke- 8 sebesar: {laba}")
+total_laba += laba
+
+print(f"Total laba adalah: {total_laba}")
